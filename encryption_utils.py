a4bd273b8865343ad6519e5c5b6ce38cf62dab45
Filip Andrei-Sorin <andrewfilip518@gmail.com>
2025-01-26 14:05:04 +0200

    Am modificat encryption.py

--- encryption_utils.py
+++ encryption_utils.py
@@ -1,4 +1,3 @@
-
 '''
 FISIERUL PENTRU FUNCTIILE DE CRIPTARE / DECRIPTARE
 '''
@@ -6,38 +5,26 @@ FISIERUL PENTRU FUNCTIILE DE CRIPTARE / DECRIPTARE
 from cryptography.fernet import Fernet
 import os
 
-
 def generate_key(key_file):
-    '''
-        Genereaza o cheie folosind Fernet, pe care o scrie apoi in format binar in
-        fisierul key_file (de unde o vom citi in viitor cand vrem sa decriptam continutul fisierului cu
-        task-urile din to-do list), si o si returneaza
-    '''
-    pass
-
+    """Generează și salvează o cheie de criptare."""
+    key = Fernet.generate_key()
+    with open(key_file, "wb") as file:
+        file.write(key)
+    return key
 
 def load_key(key_file):
-    '''
-        Daca nu exista fisierul key_file (nu s-a generat deja o cheie), apeleaza generate_key si returneaza
-        cheia rezultata. Altfel, inseamna ca exista fisierul cu cheia, deci o citeste in format binar
-        din acesta.
-    '''
-    pass
-
+    """Încarcă cheia din fișier sau o generează dacă lipsește."""
+    if not os.path.exists(key_file):
+        return generate_key(key_file)
+    with open(key_file, "rb") as file:
+        return file.read()
 
 def encrypt_data(data, key_file):
-    '''
-        Incarca cheia de criptare intr-o variabila (key, de exemplu) (folosind functia de mai sus)
-        Creaza un obiect Fernet cu cheia obtinuta
-        Cripteaza data cu metoda encrypt() a obiectului Fernet si returneaza rezultatul
-    '''
-    pass
-
+    """Criptează datele folosind cheia specificată."""
+    key = load_key(key_file)
+    return Fernet(key).encrypt(data)
 
 def decrypt_data(data, key_file):
-    '''
-        Incarca cheia de criptare intr-o variabila
-        Creeaza un obiect Fernet cu cheia obtinuta
-        Decripteaza data cu metoda decrypt() a obiectului Fernet si returneaza rezultatul
-    '''
-    pass
+    """Decriptează datele folosind cheia specificată."""
+    key = load_key(key_file)
+    return Fernet(key).decrypt(data)
